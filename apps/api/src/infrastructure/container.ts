eb3b1dfc4794c2c1176e04e8163c51ac0fe34963
todor-grigorov <t.grigorov99@gmail.com>
2026-04-27 12:10:22 +0200

    feat: update container

--- apps/api/src/infrastructure/container.ts
+++ apps/api/src/infrastructure/container.ts
@@ -1,11 +1,21 @@
 import { DevLoginUseCase } from "../application/use-cases/auth/devLogin.useCase.js";
+import { CreateDocumentUseCase } from "../application/use-cases/documents/createDocument.useCase.js";
+import { GetDocumentUseCase } from "../application/use-cases/documents/getDocument.useCase.js";
+import { ListDocumentsUseCase } from "../application/use-cases/documents/listDocuments.useCase.js";
+import { GetInvoiceUseCase } from "../application/use-cases/invoices/getInvoice.useCase.js";
+import { ListInvoicesUseCase } from "../application/use-cases/invoices/listInvoices.useCase.js";
 import { JwtTokenService } from "./auth/jwtTokenService.js";
 import { prisma } from "./persistence/prisma/prismaClient.js";
+import { DocumentRepository } from "./persistence/repositories/documentRepository.js";
+import { InvoiceRepository } from "./persistence/repositories/invoiceRepository.js";
 import { MembershipRepository } from "./persistence/repositories/membershipRepository.js";
 import { UserRepository } from "./persistence/repositories/userRepository.js";
 
 const userRepository = new UserRepository(prisma);
 const membershipRepository = new MembershipRepository(prisma);
+const documentRepository = new DocumentRepository(prisma);
+const invoiceRepository = new InvoiceRepository(prisma);
+
 const tokenService = new JwtTokenService();
 
 export const container = {
@@ -16,4 +26,14 @@ export const container = {
     membershipRepository,
     tokenService,
   ),
+
+  createDocumentUseCase: new CreateDocumentUseCase(
+    documentRepository,
+    invoiceRepository,
+  ),
+  listDocumentsUseCase: new ListDocumentsUseCase(documentRepository),
+  getDocumentUseCase: new GetDocumentUseCase(documentRepository),
+
+  listInvoicesUseCase: new ListInvoicesUseCase(invoiceRepository),
+  getInvoiceUseCase: new GetInvoiceUseCase(invoiceRepository),
 };
